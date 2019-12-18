85f863f2d20b2889ceae3a03e51009e7eb48bd7f
YairSemama <55833857+YairSemama@users.noreply.github.com>
2019-12-18 13:27:25 +0200

    a

--- src/dataStructure/NodeData.java
+++ src/dataStructure/NodeData.java
@@ -3,31 +3,34 @@ package dataStructure;
 import utils.Point3D;
 
 public class NodeData implements node_data {
-
-
+    private int key , tag;
+    private Point3D P3D;
+    private double weight = Integer.MAX_VALUE;
+    private String info;
     @Override
     public int getKey() {
-        return 0;
+        return this.key;
     }
 
     @Override
     public Point3D getLocation() {
-        return null;
+        return this.P3D;
     }
 
     @Override
     public void setLocation(Point3D p) {
-
+     Point3D s = new Point3D(p.x(),p.y(),p.z());
+     this.P3D = s ;
     }
 
     @Override
     public double getWeight() {
-        return 0;
+        return this.weight;
     }
 
     @Override
     public void setWeight(double w) {
-
+        this.weight = w;
     }
 
     @Override
@@ -42,11 +45,11 @@ public class NodeData implements node_data {
 
     @Override
     public int getTag() {
-        return 0;
+        return this.tag;
     }
 
     @Override
     public void setTag(int t) {
-
+        this.tag = t;
     }
 }
