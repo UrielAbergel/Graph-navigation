fd0637b6e43445f6aa262c7ad5a98148c3ac9212
YairSemama <55833857+YairSemama@users.noreply.github.com>
2019-12-18 13:05:14 +0200

    add the interface of node_data

---
+++ src/dataStructure/NodeData.java
@@ -0,0 +1,52 @@
+package dataStructure;
+
+import utils.Point3D;
+
+public class NodeData implements node_data {
+
+
+    @Override
+    public int getKey() {
+        return 0;
+    }
+
+    @Override
+    public Point3D getLocation() {
+        return null;
+    }
+
+    @Override
+    public void setLocation(Point3D p) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(double w) {
+
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    @Override
+    public int getTag() {
+        return 0;
+    }
+
+    @Override
+    public void setTag(int t) {
+
+    }
+}
