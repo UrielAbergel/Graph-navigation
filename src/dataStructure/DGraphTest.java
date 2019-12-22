b71d13712369ada14679a8ac5ea8c59017bb399c
UrielAbergel <54840897+UrielAbergel@users.noreply.github.com>
2019-12-22 12:51:04 +0200

    create test

---
+++ src/dataStructure/DGraphTest.java
@@ -0,0 +1,54 @@
+package dataStructure;
+
+import static org.junit.Assert.*;
+
+public class DGraphTest {
+
+    @org.junit.Test
+    public void getNode() {
+    }
+
+    @org.junit.Test
+    public void getEdge() {
+    }
+
+    @org.junit.Test
+    public void addNode() {
+    }
+
+    @org.junit.Test
+    public void connect() {
+    }
+
+    @org.junit.Test
+    public void getV() {
+    }
+
+    @org.junit.Test
+    public void getE() {
+    }
+
+    @org.junit.Test
+    public void removeNode() {
+    }
+
+    @org.junit.Test
+    public void removeEdge() {
+    }
+
+    @org.junit.Test
+    public void nodeSize() {
+    }
+
+    @org.junit.Test
+    public void edgeSize() {
+    }
+
+    @org.junit.Test
+    public void getMC() {
+    }
+
+    @org.junit.Test
+    public void stringToHash() {
+    }
+}
