fc16d6a58aa7d450cffb0338b124c95edca19757
UrielAbergel <54840897+UrielAbergel@users.noreply.github.com>
2019-12-31 17:48:22 +0200

    commit changes

--- src/Graph_GUI/GUITest.java
+++ src/Graph_GUI/GUITest.java
@@ -1,5 +1,36 @@
+package Graph_GUI;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class GUITest {
 
+    @Test
+    public void returnTheX() {
+    }
+
+    @Test
+    public void returnTheY() {
+    }
+
+    @Test
+    public void mainDraw() {
+    }
+
+    @Test
+    public void makeListInt() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void init() {
+    }
 }
